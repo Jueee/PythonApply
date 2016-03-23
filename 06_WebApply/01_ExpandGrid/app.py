42f65b80274c4981e2975b259dff5c91fedf45bc
Jueee <hellojue@foxmail.com>
2016-03-23 15:49:51 +0800

    新增01_ExpandGrid

---
+++ 06_WebApply/01_ExpandGrid/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('ExpandGridTest.html')
+
+if __name__ == '__main__':
+    app.run()
