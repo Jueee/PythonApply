768d2ea8fff18d2b9b4f1e0b1606e7a5a8d918eb
Jueee <hellojue@foxmail.com>
2016-03-25 21:10:48 +0800

    集成Bootstrap

---
+++ 07_Flask/04_Flask-Bootstrap/flask-bootstrap.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+集成Bootstrap
+要使用 Bootstrap ，最直观的办法是引入对应的css和js文件.
+在Flask中只需要安装Flask-Bootstrap，然后在模板中继承base.html就可以使用Bootstrap。
+
+安装flask-bootstrap
+(venv) $ pip2 install flask-bootstrap
+'''
+from flask.ext.bootstrap import Bootstrap
