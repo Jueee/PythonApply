b65fe9087512ae1927ca43c67fd4030862f74987
尉勇强2 <hzweiyongqiang@corp.netease.com>
2017-03-28 17:31:57 +0800

    新增w3school爬取

--- 14-scrapy/spider-w3school/w3school/w3school/spiders/w3cshool_spider.py
+++ 14-scrapy/spider-w3school/w3school/w3school/spiders/w3cshool_spider.py
@@ -1,18 +1,22 @@
+# -*- coding: utf-8 -*-
 
 import scrapy
 import logging
 from w3school.items import W3SchoolItem
 
-
+# 编写的spider必须继承自scrapy的Spider类
 class W3SchoolSpider(scrapy.Spider):
     """docstring for ClassName"""
-    
+
+    # 属性name即spider唯一名字
     name = "w3school"
     allowed_domains = ['w3school.com.cn']
+    # start_url可以理解为爬取入口
     start_urls = [
         "http://www.w3school.com.cn/xml/xml_syntax.asp"
     ]
 
+    # parse() 是对scrapy.Spider类的override
     def parse(self,response):
         items = []
         for site in response.xpath("//div[@id='navsecond']/div[@id='course']/ul[1]/li"):
